ac94caaadaca246b48888fdabb7e39fd386aa81a
Tyurin Andrey <44750239+TyurinAndrey@users.noreply.github.com>
2019-02-24 14:10:17 +0300

    Последняя редакция

--- send.php
+++ send.php
@@ -1,28 +1,47 @@
-<?php
-
+<?php 
+/*
+Форма обратной связи может получать сообщения с любых почтовых ящиков.
+Исправлена проблема кодировки при получении писем почтовым клиентом Outlook.
+Вы скачали её с сайта Epic Blog https://epicblog.net Заходите на сайт снова!
+ВНИМАНИЕ! Лучше всего в переменную myemail прописать почту домена, который использует сайт. А не mail.ru, gmail и тд.
+*/
 if(isset($_POST['submit'])){
-$to = "info@epicblog.net";; // Здесь нужно написать e-mail, куда будут приходить письма
-$from = $_POST['email']; // this is the sender's Email address
+/* Устанавливаем e-mail Кому и от Кого будут приходить письма */   
+$to = "herbarium.jewelry@yandex.ru"; // Здесь нужно написать e-mail, куда будут приходить письма   
+$from = "herbarium@herbariumjewelry.ru"; // Здесь нужно написать e-mail, от кого будут приходить письма, например no-reply(собака)epicblog.net
+ 
+/* Указываем переменные, в которые будет записываться информация с формы */
 $first_name = $_POST['first_name'];
-$subject = "Форма отправки сообщений с сайта";
-$subject2 = "Copy of your form submission";
-$tel = $first_name . " оставил сообщение:" . "\n\n" . $_POST['message'];
-$message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
-
-$headers = "From:" . $from;
-$headers2 = "From:" . $to;
-
-mail($to,$subject,$message,$headers);
-// mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
-echo "Сообщение отправлено. Спасибо Вам " . $first_name . ", мы скоро свяжемся с Вами.";
-echo "<br /><br /><a href='https://epicblog.net'>Вернуться на сайт.</a>";
-
+$email = $_POST['email'];
+$phone = $_POST['tel'];
+$message = $_POST['message'];
+$subject = "herbariumjewelry.ru";
+     
+/* Проверка правильного написания e-mail адреса */
+if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email))
+{
+show_error("<br /> Е-mail адрес не существует");
 }
-
+     
+/* Переменная, которая будет отправлена на почту со значениями, вводимых в поля */
+$mail_to_myemail = "Здравствуйте! 
+Было отправлено сообщение с сайта! 
+Имя отправителя: $first_name
+E-mail: $email
+Номер телефона: $phone
+Текст сообщения: $message
+Чтобы ответить на письмо, создайте новое сообщение, скопируйте электронный адрес и вставьте в поле Кому.";  
+     
+$headers = "From: $from \r\n";
+     
+/* Отправка сообщения, с помощью функции mail() */
+mail($to, $subject, $mail_to_myemail, $headers . 'Content-type: text/plain; charset=utf-8');
+echo "Сообщение отправлено. Спасибо Вам " . $first_name . ", мы скоро свяжемся с Вами.";
+echo "<br /><br /><a href='https://herbariumjewelry.ru'>Вернуться на сайт.</a>";
+}
 ?>
-
 <!--Переадресация на главную страницу сайта, через 3 секунды-->
 <script language="JavaScript" type="text/javascript">
-function changeurl(){eval(self.location="https://epicblog.net/index.php");}
+function changeurl(){eval(self.location="https://epicblog.net");}
 window.setTimeout("changeurl();",3000);
 </script>
